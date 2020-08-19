5d3344cc63f06cd55c3e4758e26226f30faafce8
Michael Smith <michael.smith@usace.army.mil>
2020-08-19 14:07:28 -0400

    get it working

--- validator/lib/sqlite.js
+++ validator/lib/sqlite.js
@@ -2,49 +2,46 @@
 
 
 const Cesium = require('cesium');
-const path = require('path');
-const Database = require('sqlite3');
+const Database = require('better-sqlite3');
 const bufferToJson = require('./bufferToJson');
+const util = require('./utility');
+const {ungzip} = require('node-gzip');
+const FileType = require('file-type');
 const defined = Cesium.defined;
 
 module.exports = {
     getDBReader: getDBReader
 };
 
-
 async function getDBReader(filePath)
 {
-    const dbPromise = new Promise(
-            (resolve, reject) => {
-                //const db = new Database('foobar.db', { verbose: console.log });
-                const db = new Database(filePath);
-                db.on('error', (err) => {
-                    //console.log(`Error: ${err}`);
-                    reject(err);
-                });
-                db.on('ready', () => {
-                    resolve(db);
-                });
-            }
-        )
-        .catch(() => { console.error(`Failed to read ${path.basename(filePath)} as sqlite archive`); });
+    
+    const db = new Database(util.normalizePath(filePath));
 
-    const db = await dbPromise;
     if (!db) {
-        throw Error();
+        console.log('Cant open DB');
     }
 
-    return {
-        readBinary: (path) => {
+    const readData = async (path) => {
+            const normalizedPath = util.normalizePath(path);
             const stmt = db.prepare('SELECT content FROM media WHERE key = ?');
-            result = stmt.get(path);
+            const result = stmt.get(normalizedPath);
             return result;
-        },
-        readJson: (path) => {
-            const stmt = db.prepare('SELECT content FROM media WHERE key = ?');
-            result = stmt.get(path);
-            return bufferToJson(result);
+    };
+
+    return {
+        readBinary: readData,
+        readJson: async (path) => {
+            const buffer = await readData(path);
+            const ctype = await FileType.fromBuffer(buffer.content);
+            if (ctype.mime=='application/gzip')
+            {
+                const decoded = await (await ungzip(buffer.content)).toString();
+                return bufferToJson(decoded);
+            } 
+            else {
+                 return bufferToJson(buffer);
+            }
         }
     };
 }
-
